afe73ec5f04dae09209a5dcfa386cdac6501bf97
Hektor <hcostaguzman@gmail.com>
2021-02-26 01:17:25 +0100

    a

--- 1-09/1-9-2/recursos_rc.py
+++ 1-09/1-9-2/recursos_rc.py
@@ -1,7 +1,7 @@
 # Resource object code (Python 3)
 # Created by: object code
 # Created by: The Resource Compiler for Qt version 6.0.1
-# WARNING! All changes made in this file will be lost1
+# WARNING! All changes made in this file will be lost
 
 from PySide6 import QtCore
 
